88ea24f52e4cdb5170fc87304f2d62bc9423cc0a
Jangwoochul <dncjf6869@gmail.com>
2026-04-16 17:41:56 +0900

    TOTALTEST

--- Jangwoochul/src/testjava/test01.java
+++ Jangwoochul/src/testjava/test01.java
@@ -17,7 +17,29 @@ public class test01 {
         double avg = sum / 10;
         System.out.println("평균 : " + avg);
         // 최댓값 탐색
-        double max = scores[6];
+        double max = scores[0];
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+        }
         System.out.println(max);
+
+        // 최솟값 정렬
+        for (int i = 0; i < scores.length - 1; i++) {
+            for (int j = 0; j < scores.length - 1 - i; j++) {
+                if (scores[j] > scores[j + 1]) {
+                    // swap
+                    double temp = scores[j];
+                    scores[j] = scores[j + 1];
+                    scores[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("정렬");
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
     }
 }
