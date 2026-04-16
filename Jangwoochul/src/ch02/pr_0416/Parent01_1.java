5346c85a3727b79c71a41bd8fbb027c8729a5abf
Jangwoochul <dncjf6869@gmail.com>
2026-04-16 16:39:39 +0900

    Class

---
+++ Jangwoochul/src/ch02/pr_0416/Parent01_1.java
@@ -0,0 +1,11 @@
+package ch02.pr_0416;
+
+public class Parent01_1 {
+    public void method1() {
+        System.out.println("Parent-method1()");
+    }
+
+    public void method2() {
+        System.out.println("Parent-method2()");
+    }
+}
---
+++ Jangwoochul/src/ch02/pr_0416/Parent01_2.java
@@ -0,0 +1,12 @@
+package ch02.pr_0416;
+
+public class Parent01_2 extends Parent01_1 {
+    @Override
+    public void method2() {
+        System.out.println("Child-method2()");
+    }
+
+    public void method3() {
+        System.out.println("Child-method3()");
+    }
+}
